a63d73845aa6928cb26c17f8e3404b6bf1755547
adensW <1012790248@qq.com>
2017-11-03 21:47:48 +0800

    test

--- StickyNotes/View/NotesManager.cs
+++ StickyNotes/View/NotesManager.cs
@@ -69,7 +69,7 @@ namespace StickyNotes.View
                     }
                     catch
                     {
-
+                        //
                     }
                    
                     n.Show();
